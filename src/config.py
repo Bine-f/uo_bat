a322e08086b40258924a1a82438cc412a6b8dc19
Bine-f <bine.flajnik@gmail.com>
2024-08-14 12:35:47 +0200

    paths fixed

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-NET_PATH = r"C:\Users\binef\EG\uoBat\uoBat\EG_LV_SMM_28032024.json" 
+NET_PATH = r"C:\Users\binef\EG\uo_bat\src\EG_LV_SMM_28032024.json" 
 TRAFO_NAME = "T0182 TENETIŠE"
-FOLDER_PATH =   r"C:\Users\binef\EG\uoBat\uoBat\T0182 TENETISE"
+FOLDER_PATH =   r"C:\Users\binef\EG\uo_bat\src\T0182 TENETISE"
